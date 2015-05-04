eae377cae347d05e26fdd945bfbc14b02af3b656
Andrew Lee & Chase Reckling <student@epicodus.com>
2015-05-04 12:20:36 -0700

    add all, clear, and save methods, spec pass.

--- lib/list.rb
+++ lib/list.rb
@@ -1,8 +1,22 @@
 class List
+  @@all_lists = []
   attr_reader(:name,:due_date)
+
 
   define_method(:initialize) do |attributes|
     @name = attributes[:name]
     @due_date = attributes[:due_date]
+  end
+
+  define_singleton_method(:all) do
+    @@all_lists
+  end
+
+  define_method(:save) do
+    @@all_lists.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_lists = []
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -15,4 +15,26 @@ describe('List') do
       expect(newlist.due_date()).to(eq("monday"))
     end
   end
+
+  describe('.all') do
+    it('will show all the tasks') do
+      expect(List.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('will save the tasks in the class variable') do
+      newlist = List.new({:name => "garden"})
+      newlist.save()
+      expect(List.all()).to(eq([newlist]))
+    end
+  end
+
+  describe('.clear') do
+    it('will clear all tasks from the class variable') do
+      List.new({:name => "garden"}).save()
+      List.clear()
+      expect(List.all()).to(eq([]))
+    end
+  end
 end
