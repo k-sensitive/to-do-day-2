eb89bce06fc9c6991cd3c50d06c34300663e9b29
Andrew Lee & Kim Dang <student@epicodus.com>
2015-05-05 09:58:59 -0700

    add a find function to the class, spec passes

--- spec/lists_integration_spec.rb
+++ spec/lists_integration_spec.rb
@@ -13,3 +13,13 @@ describe('adding a new list', {:type => :feature}) do
     expect(page).to have_content('Success')
   end
 end
+
+describe(".find") do
+  it('returns a list by its ID') do
+    test_list = List.new({:name => "Garden", :id => nil})
+    test_list.save()
+    test_list2 = List.new({:name => "House", :id => nil})
+    test_list2.save()
+    expect(List.find(test_list2.id())).to(eq(test_list2))
+  end
+end
